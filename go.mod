e822c32d05b77956b8c120c8728332f5dff1080b
Ahmed Roach <realahmedroach@gmail.com>
2020-08-28 08:01:25 -0400

    Switch to chi

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module sces
 go 1.15
 
 require (
+	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/jackc/pgx/v4 v4.8.1
-	github.com/julienschmidt/httprouter v1.3.0
 )
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 var TESTFIRMID = "baf78936-5986-4f24-8a40-5e11aef970c6"
@@ -14,20 +16,23 @@ func main() {
 	conn := Connect()
 	defer conn.Close()
 
-	router := httprouter.New()
+	router := chi.NewRouter()
 
-	router.GET("/firm", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	router.Use(middleware.SetHeader("Content-Type", "application/json"))
+
+	router.Get("/firm", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Add("Content-Type", "application/json")
 		fmt.Fprint(writer, List(DB_FIRM, conn))
 	})
 
-	router.GET("/firm/:uid", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	router.Get("/firm/{uid}", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Add("Content-Type", "application/json")
-		fmt.Fprint(writer, Retrieve(params.ByName("uid"), DB_FIRM, conn))
+		fmt.Fprint(writer, Retrieve(chi.URLParam(request, "uid"), DB_FIRM, conn))
 	})
 
-	router.POST("/sukuk", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	router.Post("/sukuk", func(writer http.ResponseWriter, request *http.Request) {
 		body, _ := ioutil.ReadAll(request.Body)
+		defer request.Body.Close()
 		println(string(body))
 
 		order := SukukOrder{}
